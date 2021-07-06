b3bad3eaef1b1955507ef3620fc676bc9776aed8
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-07-06 10:56:56 -0500

    Post archive pagination

--- home.php
+++ home.php
@@ -13,24 +13,29 @@ get_header();
     <main id="main" class="site-main home">
         <div class="page-content z-index-1">
 			<?php if ( have_posts() ) : ?>
-            <header class="page-header">
+                <header class="page-header">
+					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+                </header>
+                <div class="article-grid">
+					<?php
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/content', get_post_type() );
+					endwhile; ?>
+                </div>
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-            </header>
-            <div class="article-grid">
-				<?php
-				while ( have_posts() ) :
-					the_post();
-					get_template_part( 'template-parts/content', get_post_type() );
-				endwhile;
-				the_posts_navigation();
-				else :
-					get_template_part( 'template-parts/content', 'none' );
-				endif;
-				?>
-            </div>
+				the_posts_pagination( array(
+					'mid_size' => 2,
+                    'prev_text' => _(' ← Newer'),
+                    'next_text' => _('Older →')
+				) );
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
         </div>
 
     </main><!-- #main -->
